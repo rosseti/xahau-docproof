9cd59ec2cca53150dcdb5dc898adb78bef8ec2da
Andrei Rosseti <andrei@facedigital.com.br>
2025-02-26 07:58:22 -0300

    fix: renaming to nextaudit

--- xapp/src/app/nextaudit/[rAddress]/page.js
+++ xapp/src/app/nextaudit/[rAddress]/page.js
@@ -281,7 +281,7 @@ const PDFCertificateExtractor = () => {
   return (
     <div className="container mx-auto pt-5 px-4">
       <h1 className="text-4xl font-bold pb-4">
-        Docproof OpenCert
+        NextAudit
       </h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
